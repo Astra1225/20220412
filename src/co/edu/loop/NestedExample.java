685a874bd3c21b63109d96fc70037bc421f11a63
admin <admin@YD2-15>
2022-04-12 17:49:46 +0900

    0412

--- src/co/edu/loop/NestedExample.java
+++ src/co/edu/loop/NestedExample.java
@@ -26,12 +26,23 @@ public class NestedExample {
 			System.out.println();
 		}
 		*/
-		
+		/*
 		for (int j = 5; j>=1; j--) {
 			for (int i = 1; i<=j; i++) {
 				System.out.print("*");
 		}
 			System.out.println();
 	}
-}
+	*/
+		for(int j = 0; j<=5; j++) {
+		for (int i = 5; i>=0; i--) {
+			if (j<i) {
+				System.out.print(" ");
+			} else {
+				System.out.print("*");
+			}
+		}
+		System.out.println();
+		}
+	}
 }
